f4618b9be324616945b79b6fe5d45eac117b41a9
ItsLok0 <77623807+ItsLok0@users.noreply.github.com>
2025-08-26 22:47:33 +0200

    Ajout section CLI + quelques modifs apropos et index

---
+++ includes/CLI.php
@@ -0,0 +1,27 @@
+<div class="cli">
+    <div class="content blur competences">
+        <h3>Compétences</h3>
+        <ul>
+            <li><span>Langages :</span> HTML, CSS, PHP, SQL, JS</li>
+            <li><span>Envrionnement :</span> Angular, PHPMyAdmin, PostgresSQL</li>
+            <li><span>OS :</span> Linux, Windows, MacOS</li>
+            <li><span>Méthodologie :</span> UML, MCD/MLD, Agile/SCRUM</li>
+            <li><span class="underline">Connaissances des notions d'accessibilités WEB</span></li>
+        </ul>
+    </div>
+    <div class="content blur langues">
+        <h3>Langues</h3>
+        <ul>
+            <li><span>Français :</span> langue maternelle</li>
+            <li><span>Anglais :</span> B2</li>
+        </ul>
+    </div>
+    <div class="content blur interets">
+        <h3>Intérêts</h3>
+        <ul>
+            <li>Formule 1</li>
+            <li>Pratique sportive (musculation, course)</li>
+            <li>Bourse/Crypto</li>
+        </ul>
+    </div>
+</div>
--- includes/apropos.php
+++ includes/apropos.php
@@ -2,25 +2,17 @@
     <div class="content">
         <h2>À propos de moi</h2>
         <p>
-            Je m'appelle Pierre, je suis développeur web junior et je suis passionné par la création
-            de sites modernes, structurés et avec un intéret pour l'accessibilité de ces derniers.
+            Je m'appelle Pierre, je suis <span>développeur web</span> et je suis passionné par la création
+            de <span>sites modernes, structurés</span> et avec un intéret pour <span>l'accessibilité</span> de ces derniers.
         </p>
         <p>
-            J'ai découvert le développement web en terminal grâce à l'option ISN (Informatique et science du numérique).
-            Par la suite, j'ai réalisé un BTS SIO puis une licence professionnelle en informatique (DA2I) afin 
+            J'ai découvert le <span>développement web</span> en terminal grâce à l'option <span>ISN (Informatique et science du numérique)</span>.
+            Par la suite, j'ai réalisé un <span>BTS SIO</span> puis une <span>licence professionnelle en informatique (DA2I)</span> afin 
             d'améliorer mes connaissances en développement et en faire mon métier. 
         </p>
         <p>
-            Au travers de mes études, stages et premier CDI, j'ai aquis des compétences solides en HTML/CSS et JS.
-            J'ai également appris a utliser Angular (framework), Wordpress (CMS) et GitLab/GitHub (versioning)
+            Au travers de mes <span>études, stages et premier CDI</span>, j'ai acquis des compétences solides en HTML/CSS et JS.
+            J'ai également appris à utiliser <span>Angular (framework), Wordpress (CMS) et GitLab/GitHub (versioning)</span>.
         </p>
-        <h3>Compétences</h3>
-        <ul>
-            <li><span>Langages :</span> HTML, CSS, PHP, SQL, JS</li>
-            <li><span>Envrionnement :</span> Angular, PHPMyAdmin, PostgresSQL</li>
-            <li><span>OS :</span> Linux, Windows, MacOS</li>
-            <li><span>Méthodologie :</span> UML, MCD/MLD, Agile/SCRUM</li>
-            <li><span class="underline">Connaissances des notions d'accessibilités WEB</span></li>
-        </ul>
     </div>
 </div>
